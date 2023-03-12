194f33cd7365ee8456d2c4b9a613ff6f24946b85
Mike Winter <78374487+michaelwinter286@users.noreply.github.com>
2023-03-12 11:11:14 -0400

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,12 @@
+﻿SELECT CustomerName
+FROM Customers, Orders
+Where Orders.CustomerID = Customers.CustomerID
+And Orders.OrderID=10310
+
+
+
+
+SELECT Address
+FROM Products, Suppliers
+WHERE Products.SupplierID = Suppliers.SupplierID
+AND ProductID = 40
